a3b169faa2b9c6e6833c5f79c1afb6ccd64a8811
Aprajita Pandey <124031417+aprajitapandey01@users.noreply.github.com>
2023-09-14 09:18:08 +0530

    Add files via upload

---
+++ rectangle.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class rectangle
+{
+	private:
+		double width;
+		double length;
+	public:
+		double area();
+rectangle()
+{
+	cout<<"Enter width: "<<endl;
+	cin>>width;
+	cout<<"Enter length: "<<endl;
+	cin>>length;
+}
+};
+double rectangle::area()
+{
+	return width*length;
+}
+main()
+{
+	rectangle r1;
+	cout<<r1.area();
+}
